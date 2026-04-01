5ea64bfa60e22eb93dd529e567580b4156171d54
MosesK-glitch <munyorikariuki@gmail.com>
2026-04-01 13:33:49 +0300

    SOKOGARDENAPP

--- src/components/Getproducts.jsx
+++ src/components/Getproducts.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 const Getproducts = () => {
+
+  // Initialize the hools to manage the state of your application
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState('');
+  const [error, setError] = useState('');
+
+
+
   return (
     <div>Welcome to the Getproducts Page</div>
   )
 }
 
-export default Getproducts
+export default Getproducts;
